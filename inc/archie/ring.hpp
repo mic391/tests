f3b520cf06fba68d1430da9e050c041c82f17100
Attu <attu@archie>
2014-01-31 22:03:43 +0100

    refactoring

--- inc/archie/ring.hpp
+++ inc/archie/ring.hpp
@@ -9,17 +9,14 @@
 namespace archie {
 namespace detail {
 
-template<typename Tp, typename ValueType = Tp,
-  typename Pointer = Tp*, typename ConstPointer = const Tp*,
-  typename Reference = Tp&,
-  typename ConstReference = const Tp&,
+template<typename ValueType, typename Pointer = ValueType*,
+  typename ConstPointer = const ValueType*,
+  typename Reference = ValueType&,
+  typename ConstReference = const ValueType&,
   typename SizeType = std::size_t,
   typename DifferenceType = std::size_t>
-class ring_input_iterator {
+class circular {
 public:
-  enum cycle_type {
-    primal, repeated
-  };
   typedef ValueType value_type;
   typedef Pointer pointer;
   typedef ConstPointer const_pointer;
@@ -28,45 +25,58 @@ public:
   typedef SizeType size_type;
   typedef DifferenceType difference_type;
 
-  ring_input_iterator()
-    : ring_input_iterator(nullptr, nullptr) {
+  enum cycle_type {
+    primal, repeated
+  };
+
+  template<typename Iterator>
+  circular(const Iterator b, const Iterator e,
+    difference_type o)
+    : range(b, e), offset(o), cycle(primal) {
   }
 
   template<typename Iterator>
-  ring_input_iterator(const Iterator begin,
-    const Iterator end, difference_type off, cycle_type c)
-    : range(begin, end), offset(off), cycle(c) {
+  circular(const Iterator b, const Iterator e)
+    : circular(b, e, 0) {
   }
 
   template<typename Iterator>
-  ring_input_iterator(const Iterator begin,
-    const Iterator end)
-    : ring_input_iterator(begin, end, 0, cycle_type::primal) {
+  circular(const Iterator b, size_type s)
+    : circular(b, b + s, 0) {
   }
 
-  const_pointer operator->() const {
-    return position();
+  circular()
+    : circular(nullptr, nullptr) {
   }
 
-  const_reference operator*() const {
-    return *position();
+  size_type capacity() const {
+    return range.size();
   }
 
-  ring_input_iterator& operator ++() {
-    if (is_out_of_range(++offset)) reset();
-    return *this;
+  bool is_out_of_range(difference_type at) const {
+    return at < 0 || at >= capacity();
   }
 
-  bool operator ==(const ring_input_iterator& rhs) const {
-    return is_same_as(rhs);
+  bool has_same_position(const circular& rhs) const {
+    return offset == rhs.offset;
   }
 
-  bool operator !=(const ring_input_iterator& rhs) const {
-    return not is_same_as(rhs);
+  bool has_same_range(const circular& rhs) const {
+    return range == rhs.range;
   }
 
-private:
-  typedef boost::iterator_range<pointer> range_type;
+  bool has_same_cycle(const circular& rhs) const {
+    return cycle == rhs.cycle;
+  }
+
+  bool is_same_as(const circular& rhs) const {
+    return has_same_position(rhs) && has_same_range(rhs)
+      && has_same_cycle(rhs);
+  }
+
+  bool is_primal() const {
+    return cycle == cycle_type::primal;
+  }
 
   void repeate() {
     cycle = cycle_type::repeated;
@@ -77,124 +87,14 @@ private:
     repeate();
   }
 
-  bool is_out_of_range(difference_type dist) const {
-    return dist < 0 || dist >= range.size();
-  }
-
-  bool has_same_position(
-    const ring_input_iterator& rhs) const {
-    return offset == rhs.offset;
-  }
-
-  bool has_same_range(
-    const ring_input_iterator& rhs) const {
-    return range == rhs.range;
-  }
-
-  bool has_same_cycle(
-    const ring_input_iterator& rhs) const {
-    return cycle == rhs.cycle;
-  }
-
-  bool is_same_as(const ring_input_iterator& rhs) const {
-    return has_same_position(rhs) && has_same_range(rhs)
-      && has_same_cycle(rhs);
+  void increment() {
+    if (is_out_of_range(++offset)) reset();
   }
 
   pointer position() const {
     pointer ptr = range.begin();
     std::advance(ptr, offset);
     return ptr;
-  }
-
-  range_type range;
-  difference_type offset;
-  cycle_type cycle;
-};
-
-}
-template<typename Tp, class Alloc = std::allocator<Tp> >
-class ring: private Alloc {
-public:
-  typedef Alloc allocator_type;
-  typedef typename allocator_type::value_type value_type;
-  typedef typename allocator_type::pointer pointer;
-  typedef typename allocator_type::const_pointer const_pointer;
-  typedef typename allocator_type::reference reference;
-  typedef typename allocator_type::const_reference const_reference;
-  typedef typename allocator_type::size_type size_type;
-  typedef typename allocator_type::difference_type difference_type;
-  typedef boost::iterator_range<pointer> range_type;
-  typedef detail::ring_input_iterator<value_type,
-    value_type, pointer, const_pointer, reference,
-    const_reference, size_type, difference_type> iterator;
-  typedef typename iterator::cycle_type cycle_type;
-private:
-
-  class range_adapter {
-  public:
-    range_adapter(pointer p, size_type s)
-      : ptr(p), size(s) {
-    }
-    range_type get_range() const {
-      return range_type(ptr, ptr + size);
-    }
-  private:
-    pointer ptr;
-    size_type size;
-  };
-
-public:
-  ring()
-    : ring(0) {
-  }
-
-  ring(const ring& other)
-    : ring(other.size()) {
-    std::copy(other.range.begin(), other.range.end(),
-      range.begin());
-    cycle = cycle_type::repeated;
-  }
-
-  explicit ring(size_type size)
-    : range(
-      range_adapter(allocator_type::allocate(size), size).get_range()), offset(
-      0), cycle(cycle_type::primal) {
-  }
-
-  template<typename Iterator>
-  ring(Iterator first, Iterator last)
-    : ring(last - first) {
-    std::copy(first, last, range.begin());
-    cycle = cycle_type::repeated;
-  }
-
-  ~ring() {
-    allocator_type::deallocate(range.begin(), range.size());
-  }
-
-  void push_back(const_reference value) {
-    *position() = value;
-    if (is_out_of_range(++offset)) {
-      offset = 0;
-      cycle = cycle_type::repeated;
-    }
-  }
-
-  const_reference operator[](difference_type idx) const {
-    return at(idx);
-  }
-
-  size_type capacity() const {
-    return range.size();
-  }
-
-  bool empty() const {
-    return size() == 0;
-  }
-
-  size_type size() const {
-    return is_primal() ? tellp() : capacity();
   }
 
   difference_type tellp() const {
@@ -205,33 +105,6 @@ public:
     return is_primal() ? 0 : tellp();
   }
 
-  const_reference front() const {
-    return at(0);
-  }
-
-  reference front() {
-    return at(0);
-  }
-
-  const_reference back() const {
-    return at(size() - 1);
-  }
-
-  reference back() {
-    return at(size() - 1);
-  }
-
-  iterator begin() const {
-    return iterator(range.begin(), range.end(), tellg(),
-      cycle_type::primal);
-  }
-
-  iterator end() const {
-    return iterator(range.begin(), range.end(), tellp(),
-      cycle_type::primal);
-  }
-
-private:
   const_reference at(difference_type idx) const {
     return range[normalize(idx)];
   }
@@ -240,28 +113,192 @@ private:
     return range[normalize(idx)];
   }
 
-  bool is_primal() const {
-    return cycle == cycle_type::primal;
-  }
-
-  bool is_out_of_range(difference_type diff) const {
-    return diff < 0 || diff >= range.size();
-  }
-
   difference_type normalize(difference_type idx) const {
     if (is_primal()) return idx % capacity();
     else return (tellp() + idx) % capacity();
   }
 
-  pointer position() const {
-    pointer ptr = range.begin();
-    std::advance(ptr, offset);
-    return ptr;
+  pointer begin() const {
+    return range.begin();
   }
 
-  const range_type range;
+  pointer end() const {
+    return range.end();
+  }
+
+protected:
+  typedef boost::iterator_range<pointer> range_type;
+  range_type range;
   difference_type offset;
   cycle_type cycle;
+};
+
+template<typename ValueType, typename Pointer = ValueType*,
+  typename ConstPointer = const ValueType*,
+  typename Reference = ValueType&,
+  typename ConstReference = const ValueType&,
+  typename SizeType = std::size_t,
+  typename DifferenceType = std::size_t>
+class ring_input_iterator: private circular<ValueType,
+  Pointer, ConstPointer, Reference, ConstReference,
+  SizeType, DifferenceType> {
+public:
+  typedef circular<ValueType, Pointer, ConstPointer,
+    Reference, ConstReference, SizeType, DifferenceType> base;
+  typedef typename base::value_type value_type;
+  typedef typename base::pointer pointer;
+  typedef typename base::const_pointer const_pointer;
+  typedef typename base::reference reference;
+  typedef typename base::const_reference const_reference;
+  typedef typename base::size_type size_type;
+  typedef typename base::difference_type difference_type;
+  typedef typename base::cycle_type cycle_type;
+
+  ring_input_iterator()
+    : base() {
+  }
+
+  template<typename Iterator>
+  ring_input_iterator(const Iterator begin,
+    const Iterator end, difference_type offset)
+    : base(begin, end, offset) {
+  }
+
+  template<typename Iterator>
+  ring_input_iterator(const Iterator begin,
+    const Iterator end)
+    : base(begin, end) {
+  }
+
+  const_pointer operator->() const {
+    return base::position();
+  }
+
+  const_reference operator*() const {
+    return *base::position();
+  }
+
+  ring_input_iterator& operator ++() {
+    base::increment();
+    return *this;
+  }
+
+  bool operator ==(const ring_input_iterator& rhs) const {
+    return base::is_same_as(rhs);
+  }
+
+  bool operator !=(const ring_input_iterator& rhs) const {
+    return not base::is_same_as(rhs);
+  }
+};
+
+}
+
+template<typename Tp, class Alloc = std::allocator<Tp> >
+class ring: private detail::circular<
+  typename Alloc::value_type, typename Alloc::pointer,
+  typename Alloc::const_pointer, typename Alloc::reference,
+  typename Alloc::const_reference,
+  typename Alloc::size_type, typename Alloc::difference_type>,
+  private Alloc {
+public:
+  typedef Alloc allocator_type;
+
+  typedef detail::circular<
+    typename allocator_type::value_type,
+    typename allocator_type::pointer,
+    typename allocator_type::const_pointer,
+    typename allocator_type::reference,
+    typename allocator_type::const_reference,
+    typename allocator_type::size_type,
+    typename allocator_type::difference_type> base;
+
+  typedef typename base::value_type value_type;
+  typedef typename base::pointer pointer;
+  typedef typename base::const_pointer const_pointer;
+  typedef typename base::reference reference;
+  typedef typename base::const_reference const_reference;
+  typedef typename base::size_type size_type;
+  typedef typename base::difference_type difference_type;
+  typedef detail::ring_input_iterator<value_type, pointer,
+    const_pointer, reference, const_reference, size_type,
+    difference_type> iterator;
+
+public:
+  ring()
+    : base() {
+  }
+
+  ring(const ring& other)
+    : ring(other.size()) {
+    std::copy(other.range.begin(), other.range.end(),
+      base::begin());
+    base::repeate();
+  }
+
+  explicit ring(size_type size)
+    : base(allocator_type::allocate(size), size) {
+  }
+
+  template<typename Iterator>
+  ring(Iterator first, Iterator last)
+    : ring(last - first) {
+    std::copy(first, last, base::begin());
+    base::repeate();
+  }
+
+  ~ring() {
+    allocator_type::deallocate(base::begin(),
+      base::capacity());
+  }
+
+  void push_back(const_reference value) {
+    *base::position() = value;
+    base::increment();
+  }
+
+  const_reference operator[](difference_type idx) const {
+    return base::at(idx);
+  }
+
+  using base::capacity;
+  using base::tellp;
+  using base::tellg;
+
+  size_type size() const {
+    return
+      base::is_primal() ? base::tellp() : base::capacity();
+  }
+
+  bool empty() const {
+    return size() == 0;
+  }
+
+  const_reference front() const {
+    return base::at(0);
+  }
+
+  reference front() {
+    return base::at(0);
+  }
+
+  const_reference back() const {
+    return base::at(size() - 1);
+  }
+
+  reference back() {
+    return base::at(size() - 1);
+  }
+
+  iterator begin() const {
+    return iterator(base::begin(), base::end(),
+      base::tellg());
+  }
+
+  iterator end() const {
+    return iterator(base::begin(), base::end(),
+      base::tellp());
+  }
 };
 
 }
